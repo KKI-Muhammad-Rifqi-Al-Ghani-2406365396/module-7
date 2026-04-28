5b8161ef2192d74fe247fe609209b7f5a9b9a9ba
alghani46 <alghani.gucci@gmail.com>
2026-04-28 19:29:08 +0700

    [Refactoring] - Optimizing method joinStudentNames

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -5,16 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-    @Query("""
-        SELECT s
-        FROM Student s
-        ORDER BY s.gpa DESC
-        LIMIT 1
-    """)
     Optional<Student> findFirstByOrderByGpaDesc();
+
+    @Query("select s.name from Student s")
+    List<String> findAllNames();
+
 }
--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -30,13 +30,10 @@ public class StudentService {
     public Optional<Student> findStudentWithHighestGpa() {
         return studentRepository.findFirstByOrderByGpaDesc();
     }
+
     public String joinStudentNames() {
-        List<Student> students = studentRepository.findAll();
-        String result = "";
-        for (Student student : students) {
-            result += student.getName() + ", ";
-        }
-        return result.substring(0, result.length() - 2);
+        List<String> studentNames = studentRepository.findAllNames();
+        return String.join(", ", studentNames);
     }
 }
 
